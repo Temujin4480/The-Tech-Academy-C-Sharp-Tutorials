7ea01024a4c75bc03c26dbfdcd80c44671911dd9
Temujin4480 <44020596+Temujin4480@users.noreply.github.com>
2019-05-20 11:12:19 +0700

    Add files via upload

---
+++ 14. TwentyOnePart3/TwentyOnePart3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOnePart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In part 3 we're going to move the shuffle method into the class deck which makes the code much more stream lined
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
